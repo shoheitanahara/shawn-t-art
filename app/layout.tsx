d442a9bf426812c169ed61fba67ada1d30b21c00
shoheitanahara <shohei.setzer@gmail.com>
2025-02-02 00:19:47 +0900

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,9 @@ export default function RootLayout({ children }: RootLayoutProps) {
 
   return (
     <html lang="jp">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body>
         {/*
         DarkModeの実装の影響
