c0c169029742d3587387eceb6ac170221add89ce
shoheitanahara <shohei.setzer@gmail.com>
2025-02-02 01:37:37 +0900

    update About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,31 +3,31 @@ import { FC } from 'react'
 const AboutPage: FC = () => {
   return (
     <div className="container mx-auto px-8 md:px-8 py-8 max-w-4xl">
-      <h1 className="text-3xl font-bold mb-6">Shawn T. のポートフォリオ</h1>
+      <h1 className="text-3xl font-bold mb-6">Shawn T. Art</h1>
       
       <div className="mb-8">
-        <p className="text-lg italic">「未来とヴィンテージの融合」</p>
+        <p className="text-lg italic">「どのスターも普通の人から始まる」</p>
       </div>
 
       <div className="space-y-4">
-        <p>私はShawn T.です。ポップアートに強く影響を受け、2100年代の未来と1950年代のヴィンテージを融合させた独創的で不思議な絵を描いています。私の作品は、色彩豊かで視覚的に魅力的なスタイルが特徴です。</p>
+        <p>私はShawn T.です。日本で生まれ育ち、50年代のアメリカ音楽や古いハリウッド映画に強く影響を受けています。私のテーマは「どのスターも普通の人から始まる」であり、才能と努力によってスターになる人々の旅を称賛しています。私の作品は、過去の要素を新しいひねりと組み合わせた、親しみやすくも新鮮なアートと音楽を創造しています。</p>
         <p>日常の空間を鮮やかに描写し、観る人に新しい視点を提供することを目指しています。私が描くインテリアや風景、人物は、色彩のコントラストが際立ち、見る人を引き込む魅力があります。特に、私の作品に見られる大胆な色使いと幾何学的な形状は、視覚的な楽しさを生み出します。</p>
-        <p>最新技術を駆使して、高速で動作するこのサイトは、私のアートの美しさとサイトの美しさを際立たせています。作品はNFTとして公開予定で、デジタルアートの新しい可能性を探求しています。私のアートが未来のアートシーンにおいて重要な役割を果たすことを願っています。私はWebエンジニアでもあり、アートと技術の融合を追求しています。</p>
+        <p>最新技術を駆使して、高速で動作するこのサイトは、私のアートと音楽の美しさを際立たせています。作品はNFTとして公開予定で、デジタルアートの新しい可能性を探求しています。私はWebエンジニアでもあり、アートと技術の融合を追求しています。</p>
       </div>
 
       <div className="mt-8">
         <h2 className="text-2xl font-semibold mb-2">English Version</h2>
         <div className="space-y-4">
-          <p className="italic">&ldquo;A Fusion of Future and Vintage.&rdquo;</p>
-          <p>My name is Shawn T. I create unique and whimsical art that is heavily influenced by pop art, blending the futuristic visions of 2100s with the vintage aesthetics of the 1950s. My works are characterized by vibrant colors and visually captivating styles.</p>
+          <p className="italic">&ldquo;Every Star starts as a Nobody.&rdquo;</p>
+          <p>My name is Shawn T. I am an artist and musician from Japan, deeply inspired by 50s American music and classic Hollywood films. My theme, "Every Star starts as a Nobody," celebrates the journey of ordinary people who become stars through talent and hard work. I create art and music that feel both familiar and fresh, combining elements of the past with a new twist.</p>
           <p>I strive to vividly depict everyday spaces, offering viewers a fresh perspective. The interiors, landscapes, and figures I illustrate stand out with striking color contrasts, drawing the audience in with their charm. Notably, my bold use of color and geometric shapes creates a delightful visual experience.</p>
-          <p>Utilizing the latest technology, this site operates at high speed, highlighting the beauty of my art and the site itself. My works are set to be released as NFTs, exploring new possibilities in digital art. I hope my art will play a significant role in shaping the future art scene. I am also a web engineer, pursuing the fusion of art and technology.</p>
+          <p>Utilizing the latest technology, this site operates at high speed, highlighting the beauty of my art and music. My works are set to be released as NFTs, exploring new possibilities in digital art. I am also a web engineer, pursuing the fusion of art and technology.</p>
         </div>
       </div>
 
       <div className="mt-8">
-        <p className="text-lg italic">すべての画像は私たちのオリジナルであり、無断使用や権利侵害は厳禁です。これに違反した場合、法的措置を講じることがありますので、ご了承ください。</p>
-        <p className="text-lg italic">All images are our original works, and unauthorized use or infringement of rights is strictly prohibited. Legal action may be taken in case of violation, so please be aware.</p>
+        <p className="text-lg italic">すべての画像や音楽は私たちのオリジナルであり、無断使用や権利侵害は厳禁です。これに違反した場合、法的措置を講じることがありますので、ご了承ください。</p>
+        <p className="text-lg italic">All images and music are our original works, and unauthorized use or infringement of rights is strictly prohibited. Legal action may be taken in case of violation, so please be aware.</p>
       </div>
     </div>
   )
--- app/layout.tsx
+++ app/layout.tsx
@@ -11,10 +11,10 @@ interface RootLayoutProps {
 }
 
 export const metadata: Metadata = {
-  title: 'Shawn T. Art | A Fusion of Future and Vintage',
-  description: 'Shawn T. Art | Showcasing unique and whimsical art influenced by pop art, blending futuristic visions with vintage aesthetics. Fast portfolio using the latest technologies like Next.js, Vercel, Tailwind CSS, Radix UI, and more.',
+  title: 'Shawn T. Art | CryptoStars',
+  description: 'Explore the unique and imaginative world of Shawn T., a Japanese artist and musician. His work blends the vibrant energy of pop art with futuristic and vintage aesthetics. Discover a fast and seamless experience powered by cutting-edge technologies like Next.js, Vercel, and Tailwind CSS.',
   icons: {
-    icon: 'app/images/00200-495783960.png', // faviconのパスを指定
+    icon: '/favicon.ico', // faviconのパスを指定
   },
 };
 
