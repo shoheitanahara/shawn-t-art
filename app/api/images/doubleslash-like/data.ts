2e2c07e6a1eb5ce59a6a0c9ee2ec340baadef70e
Shohei Tanahara <shoheitanahara@Shoheis-MacBook-Air.local>
2025-10-20 08:17:36 +0900

    just build

--- app/api/images/doubleslash-like/data.ts
+++ app/api/images/doubleslash-like/data.ts
@@ -1,1 +1,1 @@
-export const doubleslashLikeImageList = ["like8.png","like6.png","like3.png","like2.png","like4.png","like7.png","like1.png","like5.png"];
+export const doubleslashLikeImageList = ["like1.png","like2.png","like8.png","like4.png","like3.png","like5.png","like6.png","like7.png"];
