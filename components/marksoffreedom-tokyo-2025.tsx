1c7b807775295d4119951f943d795ecf04acacd6
Shohei Tanahara <shoheitanahara@Shoheis-MacBook-Air.local>
2026-01-12 09:57:47 +0900

    fix image size

--- components/marksoffreedom-tokyo-2025.tsx
+++ components/marksoffreedom-tokyo-2025.tsx
@@ -59,7 +59,7 @@ const MarksOfFreedomTokyo2025: React.FC = () => {
             {images.map((image, index) => (
               <Card key={image} className="m-4 cursor-pointer h-50 md:h-50 lg:h-50 object-contain" onClick={() => setSelectedImage(image)}>
                 <CardContent className="grid gap-2 p-0">
-                  <Image src={image} alt={`Image ${index}`} width={400} height={300} className="object-cover h-full" />
+                  <Image src={image} alt={`Image ${index}`} width={800} height={600} className="object-cover h-full" />
                 </CardContent>
               </Card>
             ))}
